b53c91ee8df084b216c0e3ec45d2a33c39df3514
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2024-02-05 02:29:37 +0200

    add styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,14 @@
-import { Header } from 'components/Header';
+import { RoutesComp } from 'components/Routes/RoutesComp';
 
 export const App = () => {
   return (
     <>
-      <h1>WELCOME TO OUR PROJECT</h1>
-      <Header />
+      <div className="container">
+        <h1>WELCOME TO OUR PROJECT</h1>
+        <RoutesComp />
+        {/* <Header />
+        <AnyComponent /> */}
+      </div>
     </>
   );
 };
--- src/components/Header/Theme/Theme.styled.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const ThemeSelect = styled.select`
-
-
-`
