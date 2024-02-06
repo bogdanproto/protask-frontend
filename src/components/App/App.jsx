490285efae6171b5a6bac02b6148ddd0b23ac642
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2024-02-06 21:42:32 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,3 @@
-import { Header } from 'components/Header';
 import { RoutesComp } from 'components/Routes/RoutesComp';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
@@ -13,17 +12,7 @@ export const App = () => {
 
   return (
     <>
-<<<<<<< HEAD
-      <div className="container">
-        <h1>WELCOME TO OUR PROJECT</h1>
-        <RoutesComp />
-        {/* <Header /> */}
-        {/* <Header />
-        <AnyComponent /> */}
-      </div>
-=======
       <RoutesComp />
->>>>>>> f858055387599acb0eefed2edcb7b76301b54301
     </>
   );
 };
