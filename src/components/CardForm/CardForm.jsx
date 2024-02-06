c49afc9da1a371b00529f0df906c8652d0466b4e
ArtSatanov <artsatanov@gmail.com>
2024-02-06 18:28:52 +0200

    Update CardForm.jsx

--- src/components/CardForm/CardForm.jsx
+++ src/components/CardForm/CardForm.jsx
@@ -2,5 +2,37 @@ import React from 'react';
 import { Formik, Field, Form } from 'formik';
 
 export const CardForm = () => {
-  return <div>CardForm</div>;
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+        }}
+        onSubmit={async values => {
+          await new Promise(r => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field id="firstName" name="firstName" placeholder="Jane" />
+
+          <label htmlFor="lastName">Last Name</label>
+          <Field id="lastName" name="lastName" placeholder="Doe" />
+
+          <label htmlFor="email">Email</label>
+          <Field
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+          />
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
