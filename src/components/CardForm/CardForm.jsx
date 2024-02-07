b006d57ef49c1fce1a7ea22935ff6ba24ed1e6f4
ArtSatanov <artsatanov@gmail.com>
2024-02-07 19:37:39 +0200

    Update CardForm.jsx

--- src/components/CardForm/CardForm.jsx
+++ src/components/CardForm/CardForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Formik, Field, Form } from 'formik';
 
-export const CardForm = () => {
+export const CardForm = ({}) => {
   return (
     <div>
       <h1>Sign Up</h1>
