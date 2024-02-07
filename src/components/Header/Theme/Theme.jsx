76a1f525ed95fdce4ec589b6c8db8d9ccdd3beb3
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2024-02-07 19:59:05 +0200

    add

--- src/components/Header/Theme/Theme.jsx
+++ src/components/Header/Theme/Theme.jsx
@@ -10,8 +10,6 @@ export const HeaderThemeSelect = () => {
   const theme = useContext(ThemeContext);
   const color = useSelector(state => state.authUser.user.theme);
 
-console.log(theme);
-
   const [selectedOption, setSelectedOption] = useState(color);
   const dispatch = useDispatch();
   useEffect(() => {
--- src/redux/authSlice/slice/authSlice.js
+++ src/redux/authSlice/slice/authSlice.js
@@ -23,7 +23,7 @@ import { updUserTheme } from '../operations/updUserTheme';
 import { setErrorAuth, setSuccesMsgUser } from '../reducers';
 
 const initialState = {
-  user: { userName: "margo", email: "qqq@qq", avatarCloudURL: null, theme: 'light' },
+  user: {  userName: null, email: null, avatarCloudURL: null, theme: 'light' },
   token: null,
   isLoggedIn: false,
   errorAuth: null,
