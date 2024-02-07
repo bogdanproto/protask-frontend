32dc59a57328666fef195af8e67b039c1cc610a7
Bogdan Protasenko <b.protasenko@gmail.com>
2024-02-07 15:32:06 +0100

    fix data decorator

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,14 @@ import { RoutesComp } from 'components/Routes/RoutesComp';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { refreshUser } from 'redux/authSlice/operations';
+import { getAllWallpapers } from 'redux/uiSlice/operations';
 
 export const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(refreshUser());
+    dispatch(getAllWallpapers());
   }, [dispatch]);
 
   return (
