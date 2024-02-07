09c8f336f40c3670153721ca49aa47ab6fd3a676
ArtSatanov <artsatanov@gmail.com>
2024-02-08 00:55:57 +0200

    1

--- src/components/CardForm/CardForm.jsx
+++ src/components/CardForm/CardForm.jsx
@@ -1,52 +1,139 @@
 import React from 'react';
-import { Formik, Field, Form } from 'formik';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { FormAuthStyled } from 'components/Auth/common/Form/FormAuth.styled';
+import { InputBoxErr } from 'components/common';
+import { ButtonAuth } from 'components/Auth/common/ButtonAuth/ButtonAuth';
+import { useDispatch } from 'react-redux';
+import { InputCardForm } from './InputCardForm/InputCardForm';
+
+const cardSchema = Yup.object().shape({
+  title: Yup.string().required('Required'),
+  description: Yup.string().required('Required'),
+});
 
 export const CardForm = ({}) => {
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      description: '',
+      priority: '',
+    },
+    validationSchema: cardSchema,
+
+    onSubmit: values => {
+      dispatch();
+    },
+  });
   return (
     <div>
       <h1>Sign Up</h1>
-      <Formik
-        initialValues={{
-          title: '',
-          description: '',
-          priority: '',
-        }}
-        onSubmit={() => {}}
-      >
-        <Form>
-          <label htmlFor="title">First Name</label>
-          <Field id="title" name="title" placeholder="Title" />
+      <FormAuthStyled onSubmit={formik.handleSubmit}>
+        <InputBoxErr>
+          <InputCardForm
+            id="title"
+            name="title"
+            type="text"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.title}
+            placeholder="Title"
+          />
+          {formik.touched.title && formik.errors.title ? (
+            <div>{formik.errors.title}</div>
+          ) : null}
+        </InputBoxErr>
 
-          <label htmlFor="description">Last Name</label>
-          <Field
+        <InputBoxErr>
+          <InputCardForm
             id="description"
             name="description"
+            type="text"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.description}
             placeholder="Description"
           />
+          {formik.touched.description && formik.errors.description ? (
+            <div>{formik.errors.description}</div>
+          ) : null}
+        </InputBoxErr>
 
-          <div id="my-radio-group">Label color</div>
+        <InputBoxErr>
+          <InputCardForm
+            id="description"
+            name="description"
+            type="text"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.description}
+            placeholder="Description"
+          />
+          {formik.touched.description && formik.errors.description ? (
+            <div>{formik.errors.description}</div>
+          ) : null}
+        </InputBoxErr>
+
+        {/* <InputBoxErr>
+          <div id="my-radio-group">Picked</div>
           <div role="group" aria-labelledby="my-radio-group">
-            <label>
-              <Field type="radio" name="priority" value="Low" />
-              One
-            </label>
-            <label>
-              <Field type="radio" name="priority" value="Medium" />
-              Two
-            </label>
-            <label>
-              <Field type="radio" name="priority" value="High" />
-              Two
-            </label>
-            <label>
-              <Field type="radio" name="priority" value="Without" />
-              Two
-            </label>
-            <div>Picked: </div>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  name="selectedOption"
+                  value="option1"
+                  checked={formik.values.selectedOption === 'option1'}
+                  onChange={formik.handleChange}
+                />
+                Option 1
+              </label>
+            </div>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  name="selectedOption"
+                  value="option2"
+                  checked={formik.values.selectedOption === 'option2'}
+                  onChange={formik.handleChange}
+                />
+                Option 2
+              </label>
+            </div>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  name="selectedOption"
+                  value="option3"
+                  checked={formik.values.selectedOption === 'option3'}
+                  onChange={formik.handleChange}
+                />
+                Option 3
+              </label>
+            </div>
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  name="selectedOption"
+                  value="option4"
+                  checked={formik.values.selectedOption === 'option4'}
+                  onChange={formik.handleChange}
+                />
+                Option 4
+              </label>
+            </div>
           </div>
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+          {formik.touched.priority && formik.errors.priority ? (
+            <div>{formik.errors.priority}</div>
+          ) : null}
+        </InputBoxErr> */}
+
+        <ButtonAuth type="submit">Log In Now</ButtonAuth>
+      </FormAuthStyled>
     </div>
   );
 };
---
+++ src/components/CardForm/InputCardForm/InputCardForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const InputCardForm = props => {
+  return (
+    <>
+      <InputCardForm {...props} />
+    </>
+  );
+};
