ea7070a94cc53bbb00ae246e7f708b58e461d6a6
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2024-02-08 14:53:15 +0100

    changed style end close button

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,6 @@
 import { AnyComponent, Header } from 'components/Header';
+import ModalComponent from 'components/Modal/ModalComponent/ModalComponent';
+import Test from 'components/Modal/Test/Test';
 
 export const App = () => {
   return (
@@ -6,6 +8,8 @@ export const App = () => {
       <h1>WELCOME TO OUR PROJECT</h1>
       <Header />
       <AnyComponent />
+      <ModalComponent />
+      <Test/>
     </>
   );
 };
