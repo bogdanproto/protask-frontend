39063df6bfda21a45d8f73e985675f119d831fdb
Andrii111111111 <129265573+Andrii111111111@users.noreply.github.com>
2024-02-08 15:07:09 +0100

    updated and resolved conflicts

--- src/components/Modal/ModalComponent/ModalComponent.styled.js
+++ src/components/Modal/ModalComponent/ModalComponent.styled.js
@@ -29,15 +29,3 @@ export const ModalTitle = styled.h2`
 export const ModalContent = styled.p`
   color: #666;
 `;
-
-// export const ModalCloseButton = styled.button`
-//   position: absolute;
-//   top: 10px;
-//   right: 10px;
-//   background: none;
-//   border: none;
-//   color: #666;
-//   font-size: 16px;
-//   cursor: pointer;
-//   z-index: 1001;
-// `;
