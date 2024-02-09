fe1325c3d32334d1c1f16995daf93806a0880020
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2024-02-10 01:43:16 +0200

    fix

--- src/components/EditProfile/FormEditProfile/FormEditProfile.styled.js
+++ src/components/EditProfile/FormEditProfile/FormEditProfile.styled.js
@@ -1,22 +1,25 @@
-import styled from "styled-components";
-
-export const FormWrap = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+import styled from 'styled-components';
 
 export const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   gap: 14px;
-  input[disabled] {   
-    opacity: 0.5;   
-    background-color: #ccc; 
-    border:none; 
+  input[disabled] {
+    opacity: 0.5;
+    background-color: #ccc;
+    border: none;
   }
 
-    input{
-      color: black;
-    }
-`
+  input {
+    color: black;
+  }
 
+  button[disabled] {
+    opacity: 0.5;
+    background-color: grey;
+    border: none;
+  }
+  button:hover[disabled] {
+    background-color: grey;
+  }
+`;
