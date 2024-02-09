1727fb6ca659131c05ee0d510f11d8ef3c29390f
Bogdan Protasenko <129848498+bogdanproto@users.noreply.github.com>
2024-02-09 11:15:42 +0100

    Update asyncThunkDecoratorUser.js

--- src/helpers/redux/asyncThunkDecoratorUser.js
+++ src/helpers/redux/asyncThunkDecoratorUser.js
@@ -4,7 +4,6 @@ export const asyncThunkDecoratorUser = operation => async (arg, thunkAPI) => {
   try {
     return await operation(arg);
   } catch (error) {
-    console.log(error)
     return thunkAPI.rejectWithValue(handleError(error.response?.data));
   }
 };
