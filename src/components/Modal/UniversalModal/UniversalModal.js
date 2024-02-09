4d836a1c4602ad3a5d3ea0a78ed5f58cf0d4977d
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2024-02-09 18:24:38 +0200

    add NeedHelpForm

--- src/components/Modal/UniversalModal/UniversalModal.js
+++ src/components/Modal/UniversalModal/UniversalModal.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Modal from 'react-modal';
-import modalStyles from './UniversalModal.styled';
+import modalStyles, { ModalCloseButton } from './UniversalModal.styled';
+import styled from 'styled-components';
 
-const UniversalModal = ({ isOpen, onClose, children }) => {
+const UniversalModal = ({ isOpen, onClose, children, title }) => {
   return (
     <Modal
       isOpen={isOpen}
@@ -10,9 +11,45 @@ const UniversalModal = ({ isOpen, onClose, children }) => {
       style={modalStyles}
       ariaHideApp={false}
     >
+      <TopTitleModal>
+        {title}
+        <ModalCloseButton onClick={onClose}>
+          <svg
+            width="18"
+            height="18"
+            viewBox="0 0 18 18"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13.5 4.5L4.5 13.5"
+              stroke="#161616"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M4.5 4.5L13.5 13.5"
+              stroke="#161616"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </ModalCloseButton>
+      </TopTitleModal>
       {children}
     </Modal>
   );
 };
+
+export const TopTitleModal = styled.div`
+  margin-bottom: 20px;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
+  font-size: 18px;
+  letter-spacing: -0.02em;
+  color: #161616;
+`;
 
 export default UniversalModal;
--- src/components/Sidebar/Support/Support.jsx
+++ src/components/Sidebar/Support/Support.jsx
@@ -1,25 +1,33 @@
 import { SupportBox, HelpButton } from './Support.styled';
-import SupportLogoDesktop from "data/img/img_help_desktop.png";
-import SupportLogoTablet from "data/img/img_help_tablet.png";
-import SupportLogoMobile from "data/img/img_help_mobile.png";
+import SupportLogoDesktop from 'data/img/img_help_desktop.png';
+import SupportLogoTablet from 'data/img/img_help_tablet.png';
+import SupportLogoMobile from 'data/img/img_help_mobile.png';
 import { supportText } from 'data';
-import { Icon } from 'components/common/Icon/Icon'
+import { Icon } from 'components/common/Icon/Icon';
+import { useModal } from '../../../hooks/useModal';
+import { NeedHelpForm } from './NeedHelpForm';
+import UniversalModal from 'components/Modal/UniversalModal/UniversalModal';
 
-export const Support = () => { 
-    return (
-        <>
-            <SupportBox>
-                <picture>
-                    <source media="(min-width:1440px)" srcSet={SupportLogoDesktop}/>
-                    <source media="(min-width:768px)" srcSet={SupportLogoTablet}/>
-                    <img src={SupportLogoMobile} alt="Support Icon" />
-                </picture>
-                <p>{supportText}</p>
-                <HelpButton type="button">
-                    <Icon width={20} height={20} id={'icon-help_circle'}></Icon>
-                    <span>Need help?</span>
-                </HelpButton>
-            </SupportBox>
-        </>
-    )
-}
+export const Support = () => {
+  const { isOpen, open, close } = useModal();
+
+  return (
+    <>
+      <SupportBox>
+        <picture>
+          <source media="(min-width:1440px)" srcSet={SupportLogoDesktop} />
+          <source media="(min-width:768px)" srcSet={SupportLogoTablet} />
+          <img src={SupportLogoMobile} alt="Support Icon" />
+        </picture>
+        <p>{supportText}</p>
+        <HelpButton onClick={open} type="button">
+          <Icon width={20} height={20} id={'icon-help_circle'}></Icon>
+          <span>Need help?</span>
+        </HelpButton>
+        <UniversalModal title="Need help?" isOpen={isOpen} onClose={close}>
+          <NeedHelpForm />
+        </UniversalModal>
+      </SupportBox>
+    </>
+  );
+};
