c3614804e40b976788cf69fc548caca197b312d3
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2024-02-09 16:56:35 +0200

    add iconsLibrary

---
+++ src/components/common/IconsLibrary/IconWrappers.js
@@ -0,0 +1,41 @@
+export const IconWrapper16 = ({ children, size }) => (
+  <div>
+    <svg
+      width={size}
+      height={size}
+      fill="none"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {children}
+    </svg>
+  </div>
+);
+
+export const IconWrapper18 = ({ children, size }) => (
+  <div>
+    <svg
+      width={size}
+      height={size}
+      fill="none"
+      viewBox="0 0 18 18"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {children}
+    </svg>
+  </div>
+);
+
+export const IconWrapper24 = ({ children, size }) => (
+  <div>
+    <svg
+      width={size}
+      height={size}
+      fill="none"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {children}
+    </svg>
+  </div>
+);
