eb0e207fb7019a1298370d74120312e20e249ffa
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2024-02-09 13:05:57 +0200

    fix close button

--- src/components/EditProfile/index.js
+++ src/components/EditProfile/index.js
@@ -3,6 +3,3 @@ export * from './EditProfile/EditProfile.jsx';
 export * from './FormEditProfile/FormEditProfile.jsx';
 export * from './ProfileIcon/ProfileIcon.jsx';
 export * from './ProfileAvatar/ProfileAvatar.jsx'
-
-//helpers
-export * from './helpers/getNewField.js';
