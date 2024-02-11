29f6e6744aef789ce81618e03fad58b3e722e799
Bogdan Protasenko <b.protasenko@gmail.com>
2024-02-11 19:15:44 +0100

    fix route board

--- src/components/Sidebar/BoardsItem/BoardsItem.jsx
+++ src/components/Sidebar/BoardsItem/BoardsItem.jsx
@@ -5,20 +5,25 @@ import { deleteBoard } from 'redux/dataSlice/operations/board/deleteBoard';
 import UniversalModal from 'components/Modal/UniversalModal/UniversalModal';
 import { BoardForm } from 'components/BoardForm/BoardForm';
 import { useModal } from 'hooks/useModal';
+import { useNavigate } from 'react-router';
+import { routes } from 'const';
 
 export const BoardsItem = ({ board, active }) => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const { isOpen, close, toggle } = useModal();
 
   const handleDelete = evt => {
     dispatch(deleteBoard(evt.target?.closest('button').dataset.id));
+    navigate(routes.HOME);
   };
 
   return (
     <>
       <Icon width={18} height={18} id={`icon-project_${board.icon}`}></Icon>
       <h3>{board.title}</h3>
+
       {active === true && (
         <BtnContainer>
           <button type="button" data-id={board._id} onClick={toggle}>
