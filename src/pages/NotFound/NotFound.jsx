9a42d8561a261130fdf7ed80c7e3560e4ccdea48
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2024-02-12 19:30:37 +0200

    desktop styles

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
+import {
+  DogImage,
+  FlagImage,
+  Mystery,
+  NotFoundButton,
+  NotFoundStyled,
+  NotFoundTitle,
+  Paragraph404,
+  ParagraphErr,
+  ParagraphText,
+  Question,
+  SpotImage,
+} from './NotFound.styled';
+import dogImage from 'data/img/img_notFound_dog.png';
+import flagImage from 'data/img/img_notFoun_flag.png';
+import spotImage from 'data/img/Vector.png';
 
 export const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <NotFoundStyled>
+      <NotFoundTitle>Oh!</NotFoundTitle>
+      
+      <ParagraphText>We can’t sniff out the page you are looking for.</ParagraphText>
+      <ParagraphErr>Page not found</ParagraphErr>
+      <Paragraph404>404</Paragraph404>
+      <NotFoundButton>Back to home</NotFoundButton>
+      <Mystery>
+        <Question>Who are the best?</Question>
+        <FlagImage src={flagImage} width="150" alt="flag" />
+        Ukrainians are the best!!!
+      </Mystery>
+      <SpotImage src={spotImage} width="150" alt="dog" />
+      <DogImage src={dogImage} width="150" alt="dog" />
+      
+    </NotFoundStyled>
+  );
 };
---
+++ src/pages/NotFound/NotFound.styled.js
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+
+export const NotFoundStyled = styled.div`
+  position: relative;
+  overflow: hidden;
+  height: 100vh;
+  background-color: #212121;
+  color: white;
+  padding: 20px;
+  text-align: center;
+`;
+
+export const NotFoundTitle = styled.h6`
+  font-family: 'Ultra', sans-serif;
+  font-size: 60px;
+  margin-bottom: 10px;
+`;
+
+export const ParagraphText = styled.p`
+  font-size: 20px;
+  margin-bottom: 10px;
+`;
+
+export const Paragraph404 = styled.p`
+  font-weight: 600;
+  font-size: 130px;
+  margin-bottom: 10px;
+  color: #6a63fe;
+`;
+
+export const NotFoundButton = styled.button`
+  font-weight: 800;
+  font-size: 20px;
+  margin-bottom: 10px;
+  background-color: white;
+  color: #212121;
+  padding: 10px 15px;
+  margin: 0 auto;
+  border-bottom: 4px solid #fac515;
+  border-left: 1px solid #fac515;
+  border-right: 4px solid #fac515;
+  border-top: 1px solid #fac515;
+  border-radius: 6px;
+`;
+
+export const ParagraphErr = styled.p`
+  font-size: 30px;
+  margin-bottom: 10px;
+`;
+
+export const DogImage = styled.img`
+  position: absolute;
+  width: 150px;
+  bottom: 10%;
+  right: 10%;
+`;
+
+export const SpotImage = styled.img`
+  position: absolute;
+  width: 600px;
+  top: 40%;
+  left: 60%;
+`;
+
+export const Question = styled.p`
+  font-size: 20px;
+  margin-bottom: 10px;
+`;
+
+export const Mystery = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 8%;
+  font-size: 15px;
+  height: 200px;
+  width: 300px;
+`;
+
+export const FlagImage = styled.img`
+  position: absolute;
+
+  top: 35%;
+  left: 10%;
+`;
