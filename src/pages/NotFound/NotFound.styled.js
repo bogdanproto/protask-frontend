6136d78b1c84607bd8505b05c7965acb12ee54b2
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2024-02-12 21:03:01 +0200

    Update NotFound.styled.js

--- src/pages/NotFound/NotFound.styled.js
+++ src/pages/NotFound/NotFound.styled.js
@@ -9,11 +9,9 @@ export const NotFoundStyled = styled.div`
   padding: 20px;
   text-align: center;
 
-
   @media (min-width: 1440px) {
     padding: 40px;
-        }
-
+  }
 `;
 
 export const NotFoundTitle = styled.h6`
@@ -21,20 +19,18 @@ export const NotFoundTitle = styled.h6`
   font-size: 40px;
   margin-bottom: 10px;
 
- 
   @media (min-width: 1440px) {
     font-size: 60px;
-        }
+  }
 `;
 
 export const ParagraphText = styled.p`
   font-size: 15px;
   margin-bottom: 10px;
 
-
   @media (min-width: 1440px) {
     font-size: 20px;
-        }
+  }
 `;
 
 export const Paragraph404 = styled.p`
@@ -43,10 +39,9 @@ export const Paragraph404 = styled.p`
   margin-bottom: 10px;
   color: #6a63fe;
 
- 
   @media (min-width: 1440px) {
     font-size: 130px;
-        }
+  }
 `;
 
 export const NotFoundButton = styled.div`
@@ -62,14 +57,13 @@ export const NotFoundButton = styled.div`
   border-left: 1px solid #fac515;
   border-right: 4px solid #fac515;
   border-top: 1px solid #fac515;
-  border-radius: 6px;  
+  border-radius: 6px;
   cursor: pointer;
-
 
   @media (min-width: 1440px) {
     font-size: 20px;
     margin-bottom: 10px;
-        }
+  }
 `;
 
 export const ParagraphErr = styled.p`
@@ -78,7 +72,7 @@ export const ParagraphErr = styled.p`
 
   @media (min-width: 1440px) {
     font-size: 30px;
-        }
+  }
 `;
 
 export const DogImage = styled.img`
@@ -93,7 +87,7 @@ export const DogImage = styled.img`
 
   @media (min-width: 1440px) {
     width: 150px;
-        }
+  }
 `;
 
 export const SpotImage = styled.img`
@@ -103,26 +97,24 @@ export const SpotImage = styled.img`
   left: 60%;
 `;
 
-export const Question = styled.p`
-  font-size: 16px;
-  margin-bottom: 8px;
-  
-
-  @media (min-width: 1440px) {
-    font-size: 20px;
-    margin-bottom: 10px;
-        }
-`;
-
 export const Mystery = styled.div`
   position: absolute;
   top: 50%;
   left: 8%;
   text-align: left;
- `;
+`;
+
+export const Question = styled.p`
+  font-size: 16px;
+  margin-bottom: 8px;
+
+  @media (min-width: 1440px) {
+    font-size: 20px;
+    margin-bottom: 10px;
+  }
+`;
 
 export const Answer = styled.p`
-  
   font-size: 12px;
   height: 200px;
   width: 300px;
@@ -131,7 +123,7 @@ export const Answer = styled.p`
   @media (min-width: 1440px) {
     font-size: 20px;
     margin-bottom: 10px;
-        }
+  }
 `;
 
 export const FlagImage = styled.img`
