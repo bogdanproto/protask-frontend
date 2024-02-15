807ed32ecefb1c2bc3050c1c883b7d61cddd7323
Igor Nagrebelnyi <nagrebelnyi.igor@gmail.com>
2024-02-15 14:51:42 +0200

    fix errors

--- src/components/common/IconsLibrary/Icons.js
+++ src/components/common/IconsLibrary/Icons.js
@@ -3,7 +3,7 @@ import {
   IconWrapper18,
   IconWrapper20,
   IconWrapper28,
-  IconWrapper28wh,
+  // IconWrapper28wh,
   IconWrapper32,
 } from './IconWrappers';
 
@@ -46,7 +46,7 @@ export const HelpIcon = ({ size }) => {
         stroke="currentColor"
         strokeWidth="1.8"
         strokeLinecap="round"
-        stroke-Linejoin="round"
+        strokeLinejoin="round"
       />
       <path
         d="M7.57495 7.49999C7.77087 6.94304 8.15758 6.47341 8.66658 6.17426C9.17558 5.87512 9.77403 5.76577 10.3559 5.86558C10.9378 5.96539 11.4656 6.26792 11.8458 6.71959C12.2261 7.17126 12.4342 7.74292 12.4333 8.33332C12.4333 9.99999 9.93328 10.8333 9.93328 10.8333"
